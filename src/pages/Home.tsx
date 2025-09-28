62d3b142f5197c7025634dd5b7bbddefbdb4181a
Otavio <otavio.zerotwo@gmail.com>
2025-09-28 17:22:05 -0300

    refactor: separação card adionar item da page home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,14 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card.tsx";
+import { AddItem } from "@/sectons/AddItem.tsx";
 import { Header } from "../components/header.tsx";
-import { Container } from "@/components/container.tsx";
-import { Input } from "@/components/ui/input.tsx";
-import { Button } from "@/components/ui/button.tsx";
-import { Plus } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-      <div className="mt-6">
-        <Container>
-          <Card>
-            <CardHeader>
-              <CardTitle>Adicionar Item</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <div className="flex items-center gap-2">
-                  <Input 
-                    type="text" 
-                    placeholder="Digite o nome do item" 
-                    className="h-10" 
-                    required
-                  />
-                  <Button 
-                    variant="default" 
-                    size="sm" 
-                    className="h-10 cursor-pointer"
-                  >
-                    <Plus color="#FFF" />
-                    Adicionar
-                  </Button>
-                </div>
-              </form>
-            </CardContent>
-          </Card>
-        </Container>
-      </div>
+      <section className="mt-6">
+        <AddItem />
+      </section>
     </div>
   );
 }
---
+++ src/sectons/AddItem.tsx
@@ -0,0 +1,37 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card.tsx";
+import { Container } from "@/components/container.tsx";
+import { Input } from "@/components/ui/input.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { Plus } from "lucide-react";
+
+export function AddItem() {
+  return (
+    <Container>
+      <Card>
+        <CardHeader>
+          <CardTitle>Adicionar Item</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex items-center gap-2">
+              <Input 
+                type="text" 
+                placeholder="Digite o nome do item" 
+                className="h-10" 
+                required
+              />
+              <Button 
+                variant="default" 
+                size="sm" 
+                className="h-10 cursor-pointer"
+              >
+                <Plus color="#FFF" />
+                Adicionar
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+}
