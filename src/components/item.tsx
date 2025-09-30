e0db387dc929611c62a5773945aa3a06cc56713c
Otavio <otavio.zerotwo@gmail.com>
2025-09-29 21:39:54 -0300

    feat: add checkbox e buttons card component

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,5 +1,7 @@
+import { FilePenLine, Trash2 } from "lucide-react";
 import { Container } from "./container";
 import { Card, CardContent } from "./ui/card";
+import { Checkbox } from "./ui/checkbox";
 
 interface ItemProps {
   id: number;
@@ -17,8 +19,19 @@ export function Item({ items }: ListItemProps) {
         {items.map((item: ItemProps) => (
           <li key={item.id} className="mb-3">
             <Card>
-              <CardContent>
-                {item.text}
+              <CardContent className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                  <Checkbox className="h-5 w-5 rounded-full border-2 border-gray-300" />
+                  {item.text}
+                </div>
+                <div className="flex gap-2 text-gray-600">
+                  <button className="cursor-pointer">
+                    <FilePenLine />
+                  </button>
+                  <button className="cursor-pointer">
+                    <Trash2 />
+                  </button>
+                </div>
               </CardContent>
             </Card>
           </li>
